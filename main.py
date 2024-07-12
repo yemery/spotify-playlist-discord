ef308f4410ada953d6ddc4b997c2b819c134a20b
meryem <meryemajmani33@gmail.com>
2024-07-12 01:12:16 +0100

    OBSTACLE:spotify api limit in getting tracks of a playlist to 100 && added main func to check diff in playlists

--- main.py
+++ main.py
@@ -1,32 +1,59 @@
 from db import get_db_connection
 from utils import get_playlist_info
-# import schedule
 
+# import schedule
+import pandas as pd
 
 
 # checking if new tracks been added to any playlist in db
 def check_new_tracks():
     try:
         db = get_db_connection()
-        playlists= db["playlists"].find()
+        playlists = db["playlists"].find()
         for playlist in playlists:
+            print("*" * 50)
             playlist_id = playlist.get("id")
             playlist_data = get_playlist_info(playlist_id)
-            if playlist_data.get("total_tracks") > playlist.get("total_tracks"):
-                print(f"New tracks added to {playlist_data.get('name')} playlist")
-                # update the db with the new tracks
-                db["playlists"].update_one({"id": playlist_id}, {"$set": {"total_tracks": playlist_data.get("total_tracks")}})
-            else:
-                print(f"No new tracks added to {playlist_data.get('name')} playlist")
-        
+            print(playlist_data.get("tracks").get("total"))
+            print(playlist.get("tracks").get("total"))
+            # if playlist_data.get("total_tracks") > playlist.get("total_tracks"):
+                # using pandas to check if new tracks been added or track been removed
+                # get items from both old and new playlist
+                # items= playlist_data.get('tracks', {}).get('items', [])
+                # for i in items:
+                #     print(i.get('track').get('name'))
+            old_tracks = pd.DataFrame(playlist.get("tracks", {}).get("items", []))
+            new_tracks = pd.DataFrame(
+                playlist_data.get("tracks", {}).get("items", [])
+            )
+            print(old_tracks)
+            print(new_tracks)
+            # indexing by name to compare the two dataframes its in track name level i'll use addded at till find optimizd sol to access to it directly in one line
+            old_tracks.set_index(old_tracks['track'].apply(lambda x: x.get('id')), inplace=True)
+            new_tracks.set_index(new_tracks['track'].apply(lambda x: x.get('id')), inplace=True)
             
-        
+            # check if new tracks been added or track been removed
+            added = new_tracks[~new_tracks.index.isin(old_tracks.index)]
+            removed = old_tracks[~old_tracks.index.isin(new_tracks.index)]
+            print(added)
+            print(removed)
+
+           
+                # print infos about the new tracks added and removed
+                # print(f"New tracks added to {playlist_data.get('name')} playlist")
+                # print(added)
+                # print(f"Tracks removed from {playlist_data.get('name')} playlist")
+                # print(removed)
+            #
+            # print(f"New tracks added to {playlist_data.get('name')} playlist")
+
+            # # update the db with the new tracks
+            # db["playlists"].update_one({"id": playlist_id}, {"$set": {"total_tracks": playlist_data.get("total_tracks")}})
             
+
     except Exception as e:
         print("Error in checking new tracks")
         print(e)
-
-
 
 
 check_new_tracks()
