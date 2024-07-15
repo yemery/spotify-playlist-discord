96c1a37e3c293798ed25fefa278e0d228ce49f19
meryem <meryemajmani33@gmail.com>
2024-07-15 11:34:52 +0100

    testing checking of getting data about deleted or added tracks to playlists

--- main.py
+++ main.py
@@ -17,38 +17,48 @@ def check_new_tracks():
             print(playlist_data.get("tracks").get("total"))
             print(playlist.get("tracks").get("total"))
             # if playlist_data.get("total_tracks") > playlist.get("total_tracks"):
-                # using pandas to check if new tracks been added or track been removed
-                # get items from both old and new playlist
-                # items= playlist_data.get('tracks', {}).get('items', [])
-                # for i in items:
-                #     print(i.get('track').get('name'))
+            # using pandas to check if new tracks been added or track been removed
+            # get items from both old and new playlist
+            # items= playlist_data.get('tracks', {}).get('items', [])
+            # for i in items:
+            #     print(i.get('track').get('name'))
             old_tracks = pd.DataFrame(playlist.get("tracks", {}).get("items", []))
-            new_tracks = pd.DataFrame(
-                playlist_data.get("tracks", {}).get("items", [])
-            )
+            new_tracks = pd.DataFrame(playlist_data.get("tracks", {}).get("items", []))
             print(old_tracks)
             print(new_tracks)
             # indexing by name to compare the two dataframes its in track name level i'll use addded at till find optimizd sol to access to it directly in one line
-            old_tracks.set_index(old_tracks['track'].apply(lambda x: x.get('id')), inplace=True)
-            new_tracks.set_index(new_tracks['track'].apply(lambda x: x.get('id')), inplace=True)
-            
+            old_tracks.set_index(
+                old_tracks["track"].apply(lambda x: x.get("id")), inplace=True
+            )  # lamda for each row in the dataframe to get the track id
+            new_tracks.set_index(
+                new_tracks["track"].apply(lambda x: x.get("id")), inplace=True
+            )
+
             # check if new tracks been added or track been removed
             added = new_tracks[~new_tracks.index.isin(old_tracks.index)]
             removed = old_tracks[~old_tracks.index.isin(new_tracks.index)]
+            print("added")
             print(added)
+            # test converting dataframe to list of dicts
+            print("list of dict")
+            # print(added.values.tolist())
+            print(added.to_dict("records"))
+
+            print("removed")
             print(removed)
             # limit of api is 100 songs well use offset and next to get all songs
-            
-            
-            
 
             # # update the db with the new tracks
             # db["playlists"].update_one({"id": playlist_id}, {"$set": {"total_tracks": playlist_data.get("total_tracks")}})
-            
 
     except Exception as e:
         print("Error in checking new tracks")
         print(e)
 
 
-check_new_tracks()
+if __name__ == "__main__":
+    check_new_tracks()
+    # schedule.every(10).seconds.do(check_new_tracks)
+    # while True:
+    #     schedule.run_pending()
+    #     time.sleep(1)
--- utils.py
+++ utils.py
@@ -43,9 +43,9 @@ def get_playlist_info(playlist_id):
         "name": response.json().get("name"),
         # "description": response.json().get("description"),
         # "followers": response.json().get("followers").get("total"),
-        "public": response.json().get("public"),
-        "type": response.json().get("type"),
-        "collaborative": response.json().get("collaborative"),
+        # "public": response.json().get("public"),
+        # "type": response.json().get("type"),
+        # "collaborative": response.json().get("collaborative"),
         "total_tracks": response.json().get("tracks").get("total"),
         "tracks": response.json().get("tracks"),
         "owner": response.json().get("owner").get("display_name"),
@@ -63,7 +63,7 @@ def get_playlist_info(playlist_id):
             offset += limit
             # next_url = f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks?offset={offset}&limit={limit}'
             response = req.get(next_page, headers=headers)
-
+            # cleanup items to store only needed data
             data["tracks"]["items"].extend(response.json()['items'])
             # update next_page to check if there is more pages
             next_page = response.json()["next"]
@@ -71,9 +71,9 @@ def get_playlist_info(playlist_id):
             # if next_page is not None:
                 
                
-    print(data['tracks']['total'])
-    print(len(data["tracks"]["items"]))
-    # return data
+    # print(data['tracks']['total'])
+    # print(len(data["tracks"]["items"]))
+    return data
 
 
 
